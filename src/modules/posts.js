26ee8e0c65972b1c2bf37f038a37c82ddd830d47
parkjeyoung <dkfmwps@daum.net>
2022-02-13 21:22:32 +0900

    군더더기 삭제2

--- src/modules/posts.js
+++ src/modules/posts.js
@@ -7,11 +7,6 @@ const GET_POSTS = "GET_POSTS"; // 요청 시작
 const GET_POSTS_SUCCESS = "GET_POSTS_SUCCESS"; // 요청 성공
 const GET_POSTS_ERROR = "GET_POSTS_ERROR"; // 요청 실패
 
-// 포스트 하나 조회하기
-const GET_POST = "GET_POST";
-const GET_POST_SUCCESS = "GET_POST_SUCCESS";
-const GET_POST_ERROR = "GET_POST_ERROR";
-
 // thunk 를 사용 할 때, 꼭 모든 액션들에 대하여 액션 생성함수를 만들 필요는 없습니다.
 // 그냥 thunk 함수에서 바로 액션 객체를 만들어주어도 괜찮습니다.
 
@@ -25,24 +20,8 @@ export const getPosts = () => async (dispatch) => {
     }
 };
 
-// thunk 함수에서도 파라미터를 받아와서 사용 할 수 있습니다.
-export const getPost = (id) => async (dispatch) => {
-    dispatch({ type: GET_POST }); // 요청이 시작됨
-    try {
-        const post = await postsAPI.getPostById(id); // API 호출
-        dispatch({ type: GET_POST_SUCCESS, post }); // 성공
-    } catch (e) {
-        dispatch({ type: GET_POST_ERROR, error: e }); // 실패
-    }
-};
-
 const initialState = {
     posts: {
-        loading: false,
-        data: null,
-        error: null,
-    },
-    post: {
         loading: false,
         data: null,
         error: null,
@@ -73,33 +52,6 @@ export default function posts(state = initialState, action) {
             return {
                 ...state,
                 posts: {
-                    loading: true,
-                    data: null,
-                    error: action.error,
-                },
-            };
-        case GET_POST:
-            return {
-                ...state,
-                post: {
-                    loading: true,
-                    data: null,
-                    error: null,
-                },
-            };
-        case GET_POST_SUCCESS:
-            return {
-                ...state,
-                post: {
-                    loading: true,
-                    data: action.post,
-                    error: null,
-                },
-            };
-        case GET_POST_ERROR:
-            return {
-                ...state,
-                post: {
                     loading: true,
                     data: null,
                     error: action.error,
